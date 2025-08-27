696c0ea8bca14a91e1ceaf4bf1739c4aa13cde63
62hoon99 <ske06008@gmail.com>
2025-08-27 19:37:26 +0900

    feat: 댓글 최대 2Depth CUD API 구현

---
+++ service/comment/src/main/java/khyou/board/comment/controller/CommentController.java
@@ -0,0 +1,31 @@
+package khyou.board.comment.controller;
+
+import khyou.board.comment.service.CommentService;
+import khyou.board.comment.service.request.CommentCreateRequest;
+import khyou.board.comment.service.response.CommentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/v1/comments/{commentId}")
+    public CommentResponse read(
+            @PathVariable("commentId") Long commentId
+    ) {
+        return commentService.read(commentId);
+    }
+
+    @PostMapping("/v1/comments")
+    public CommentResponse create(@RequestBody CommentCreateRequest request) {
+        return commentService.create(request);
+    }
+
+    @DeleteMapping("/v1/comments/{commentId}")
+    public void delete(@PathVariable("commentId") Long commentId) {
+        commentService.delete(commentId);
+    }
+
+}
